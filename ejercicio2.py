e4df9615f7b4182fe6cff7551a4c50504a899932
{JoseMunozMTP} <{jose.munoz@mtp.es}>
2023-07-04 16:22:41 +0200

    Ejercicio 2 resuelto

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,8 @@
 for numero in range(0,101):
 
-    if numero % 3 == 0:
+    if numero % 3 == 0 and numero % 5 == 0:
+        print ('Fizz - Buzz')
+    elif numero % 3 == 0:
         print ('Fizz')
     elif numero % 5 == 0:
         print('Buzz')
