f3bba77dad2d380d0105861da9003c3320ef2b5e
{JoseMunozMTP} <{jose.munoz@mtp.es}>
2023-07-06 16:39:06 +0200

    Resolución variante ejercicio Selenium 050723

--- ejercicio3Selenium2.py
+++ ejercicio3Selenium2.py
@@ -23,18 +23,36 @@ CheckBox_2 = data['checkbox 2']
 driver = webdriver.Chrome()
 driver.maximize_window()
 
+
 try:
     driver.get('https://the-internet.herokuapp.com/checkboxes')
 
-    if CheckBox_1 == True and driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[1]').is_selected() != True:
+    element_CheckBox_1 =driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[1]')
+    element_CheckBox_2 =driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[2]')
+
+    """if CheckBox_1 == True and driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[1]').is_selected() != True:
         elemento_checkbox_1 = driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[1]').click()
     elif CheckBox_1 == False and driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[1]').is_selected() == True:
-        elemento_checkbox_1 = driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[1]').click()
+        elemento_checkbox_1 = driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[1]').click()"""
 
-    if CheckBox_2 == True and driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[2]').is_selected() != True:
+    if CheckBox_1:
+        if element_CheckBox_1.is_selected() != True:
+            element_CheckBox_1.click()
+    else:
+        if element_CheckBox_1.is_selected():
+           element_CheckBox_1.click()
+
+    """if CheckBox_2 == True and driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[2]').is_selected() != True:
         elemento_checkbox_2 = driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[2]').click()
     elif CheckBox_2 == False and driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[2]').is_selected() == True:
-        elemento_checkbox_2 = driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[2]').click()
+        elemento_checkbox_2 = driver.find_element(By.XPATH, '//*[@id="checkboxes"]/input[2]').click()"""
+
+    if CheckBox_2:
+        if element_CheckBox_2.is_selected() != True:
+            element_CheckBox_2.click()
+    else:
+        if element_CheckBox_2.is_selected():
+            element_CheckBox_2.click()
 finally:
     time.sleep(3)
-    driver.close()
+    driver.quit()
