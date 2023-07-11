45b0cad5feef5b596a13de971301d9e2d1b46b0a
{JoseMunozMTP} <{jose.munoz@mtp.es}>
2023-07-11 15:03:34 +0200

    Ejercicio11 resuelto

--- ejercicio11/enviroment.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-
-def before_scenario(Context, scenario):
-    Context.driver = webdriver.Chrome()
-
-
-def after_scenario(Context, scenario):
-    Context.driver.quit()
